645cfd7970c1204678d7c625d3f7ee1ded159ced
manava10 <mmanavp10@gmail.com>
2025-01-08 15:53:11 +0530

    Step 5 , Admin end point added

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const{userRouter} = require("./route/user");
 const{courseRouter} = require("./route/course");
+const{adminRouter} = require("./route/admin");
 
 const app = express();
 
 app.use("/user",userRouter);
 app.use("/course",courseRouter)
+app.use("/admin",adminRouter);
 
 app.listen(3000,()=>{
     console.log("Server is running on the PORT 3000")
---
+++ route/admin.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const Router = express.Router;
+
+const adminRouter = Router();
+
+adminRouter.post("/signup",(req,res)=>{
+    res.json({
+        message:"Admin signUp Route"
+    })
+})
+
+adminRouter.post("/login",(req,res)=>{
+    res.json({
+        message:"Admin login Router"
+    })
+})
+
+
+//Admin can create a course . To create a course admin must be signed in before
+
+adminRouter.post("/course",(req,res)=>{
+    res.json({
+        message:"This Routes is for admin to create a course"
+    })
+})
+adminRouter.put("/course",(req,res)=>{
+    res.json({
+        message : "This route is to change the price of courses"
+        
+    })
+    
+})
+adminRouter.get("/course/bulk",(req,res)=>{
+    res.json({
+        message:"This endpoint is to Retrieve all the courses created by admin "
+    })
+})
+
+
+module.exports = {
+    adminRouter:adminRouter
+
+}
